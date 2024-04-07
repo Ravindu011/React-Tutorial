a382324345787c2674412d6036af9460cfe871cf
Ravindu C Rathnayake <ravinduc2001@gmail.com>
2024-04-07 12:53:57 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -2,72 +2,19 @@ import './App.css';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
 import HomePage from './Pages/HomePage';
-import { useState } from 'react';
-import Accordion from 'react-bootstrap/Accordion';
 import { BrowserRouter as Router,Route,Switch} from 'react-router-dom';
 import NavBar1 from './Components/NavBar1';
 
 
 function App() {
 
-  //let count = 0;
-  const [count, setCount] = useState(0);
-
-  const increment = () =>{
-     //count +=1;
-    setCount(count+1);
-  };
- 
-
-  const decrement = () =>{
-    //count-+1;
-    setCount(count-1);
-  };
   
   return (
-    
-    <Router>
+    <div>
       <NavBar1/>
-      <div>
-        <h1>React Bootstrap </h1>
-        <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Item 1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="2">
-        <Accordion.Header>Accordion Item #3</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+      
+   <Router>
+
         <Switch>
 
           <Route exact path="/">
@@ -83,17 +30,11 @@ function App() {
           </Route>
           
         </Switch>
-
-        <br/><br/><br/><br/>
-      <div>
-        <span className='title'>My Counter</span>
-        <p className='subTitle'>The count is is {count}</p>
-        <button className='btn' onClick={decrement}>-</button>
-        <span>  or   </span>
-        <button className='btn' onClick={increment}>+</button>
-      </div>
-      </div>
+     
     </Router>
+    </div>
+    
+    
 
     
   );
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import './HomePage.css';
-
+import { useState } from 'react';
+import Accordion from 'react-bootstrap/Accordion';
 import TopicBox from '../Components/TopicBox'
-import Content from '../Components/Content'
 import { Link } from 'react-router-dom'
 
 export default function HomePage() {
+  //let count = 0;
+  const [count, setCount] = useState(0);
+
+  const increment = () =>{
+     //count +=1;
+    setCount(count+1);
+  };
+ 
+
+  const decrement = () =>{
+    //count-+1;
+    setCount(count-1);
+  };
+  
   
   return (
     <div>
@@ -22,6 +36,61 @@ export default function HomePage() {
 
       <Link to="/About"> Go to the About us</Link>
       <Link to="/Contact"> Go to the Contact us</Link>
+
+      <hr/>
+
+      <div>
+     <span className='title'>My Counter</span>
+     <p className='subTitle'>The count is is {count}</p>
+     <button className='btn' onClick={decrement}>-</button>
+     <span>  or   </span>
+     <button className='btn' onClick={increment}>+</button>
+   </div>
+
+   <hr/>
+   <div>
+        <h1>React Bootstrap </h1>
+        <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Item 1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Accordion Item #3</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+    </div>
+
+
 
       
       
